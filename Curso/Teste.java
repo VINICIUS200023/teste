54932b5ea100eebf5ac8373c46a40602bd65db4c
VINICIUS200023 <125417122+VINICIUS200023@users.noreply.github.com>
2023-02-23 14:27:40 -0300

    Create Teste.java

---
+++ Curso/Teste.java
@@ -0,0 +1,17 @@
+package Curso;
+
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Scanner le = new Scanner(System.in);
+
+        System.out.println("INFORME SEU NOME");
+        String nome = le.nextLine();
+
+        System.out.println("SEU NOME "+nome);
+
+    }
+
+}
